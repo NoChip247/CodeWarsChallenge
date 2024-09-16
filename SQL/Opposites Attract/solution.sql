5cf962825216efff5b5f380a1073557b76e3aade
NoChip247 <156658200+NoChip247@users.noreply.github.com>
2024-09-16 12:47:44 +0200

    Create solution.sql

---
+++ SQL/Opposites Attract/solution.sql
@@ -0,0 +1,9 @@
+SELECT
+  flower1,
+  flower2,
+  CASE 
+    WHEN flower1 % 2 = 0 AND flower2 % 2 <> 0 THEN true
+    WHEN flower1 % 2 <> 0 AND flower2 % 2 = 0 THEN true
+    ELSE false
+  END AS res
+FROM love
